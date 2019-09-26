84be524fd628a4919d5e8b468dfb0f3a14492b2f
weisen <weisen@kedacom.com>
2019-09-26 10:10:45 +0800

    init mojia-cli

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const TEMPLATE_GIT_REPO = 'direct:https://github.com/vison123/vue-mobile-template.git';
+const INJECT_FILES = ['package.json'];
+module.exports = {
+	TEMPLATE_GIT_REPO,
+	INJECT_FILES
+};
